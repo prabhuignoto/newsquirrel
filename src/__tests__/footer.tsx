32151283aa0358b3e30dda5c05a0fde35a96f768
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2018-08-31 00:28:12 +0530

    fixing test cases

--- src/__tests__/footer.tsx
+++ src/__tests__/footer.tsx
@@ -2,9 +2,10 @@ import 'jest-dom/extend-expect';
 import * as React from 'react';
 import {render} from 'react-testing-library';
 import Footer from '../components/footer/footer';
+import { AppMode } from '../enums/appMode';
 
 test('Footer renders correctly', () => {
-  const {getByTestId} = render(<Footer />);
+  const {getByTestId} = render(<Footer appMode={{name: 'Dark', value:AppMode.DARK}}/>);
   const node = getByTestId('rt-footer');
   const link = node.querySelector('a');
   expect(node).toBeInTheDocument();
