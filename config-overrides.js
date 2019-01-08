f10285520d8071af489d15aae602a87432eb3697
Prabhu Murthy <prabhu.m.murthy@gmail.com>
2019-01-09 00:28:44 +0530

    upgraded packages

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,9 @@
-const rewireReactHotLoader = require('react-app-rewire-hot-loader');
-const rewireInlineImportGraphqlAst = require('react-app-rewire-inline-import-graphql-ast');
+// const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+// const rewireInlineImportGraphqlAst = require('react-app-rewire-inline-import-graphql-ast');
 
 module.exports = function override(config, env) {
-  config = rewireInlineImportGraphqlAst(config, env);
-  config = rewireReactHotLoader(config, env);
+  // config = rewireInlineImportGraphqlAst(config, env);
+  // config = rewireReactHotLoader(config, env);
 
   return config;
 }
